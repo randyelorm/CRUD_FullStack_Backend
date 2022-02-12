ca9472565e047d86e15f36ff5da0003e60942ab6
Randy Davoh <randyelorm@yahoo.com>
2022-02-12 16:18:55 +0000

    another fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json())
 
 dotenv.config({ path: "config.env" })
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.use("/https://crudbackend.netlify.app/", require("./SERVER/Router/Routes"))
 
